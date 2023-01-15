35f6818af1663c1e47945340069038f3997bcc5f
marcosmorandi <marcosamorandi@gmail.com>
2023-01-15 11:33:00 -0300

    Alteração no main.js.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const milissegundosPomodoro = 4000 // Estamos usando 4 segundos para testes.
+const milissegundosPomodoro = 4000 // Estamos usando 4 segundos para testes. O tempo oficial de 25 é 25*60*1000.
 const disparador = document.querySelector('#disparador')
 let milissegundosRestantes = milissegundosPomodoro - 1000
 let contador
